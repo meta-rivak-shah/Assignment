abaca0e43697442845415ef0530ecfa6d7612c09
meta-rivak-shah <52689827+meta-rivak-shah@users.noreply.github.com>
2019-07-16 16:03:59 +0530

    Update Main.java

--- assignment4/Result/Main.java
+++ assignment4/Result/Main.java
@@ -1,40 +1,23 @@
 package grades;
-
 import java.util.Scanner;
-
 public class Main {
 	public static void main(String args[]){
-		
 		Scanner userInput  =  new Scanner(System.in);  
-		
 		MarkSheet marksheezt = new MarkSheet();
-		
 		System.out.println("Enter number of students");
-		 
 		int num= userInput.nextInt();
-		
 		float []grades = new float[num];
-		
 		for(int i=0 ; i < num ; i++){
-			
-			System.out.println("Enter grade of "+ (i+1) +" student");
-			
+			System.out.println("Enter grade of "+ (i+1) +" student")
 			grades[i] = userInput.nextFloat();
 		}
 		float average = marksheet.average(grades);
-		
 		float maxgrade = marksheet.maxGrade(grades);
-		
 		float mingrade = marksheet.minGrade(grades);
-		
 		float perstudentpass = marksheet.percentageStudentPass(grades);
-		
 		System.out.println("Average of grades=" + average);
-		
 		System.out.println("Maximum grade is=" + maxgrade);
-		
 		System.out.println("Minimum grade is=" + mingrade);
-		
 		System.out.println("Percentage of students passed=" + perstudentpass+"%");
 	}
 }
