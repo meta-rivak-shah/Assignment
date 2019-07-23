83d9e358efd88a483ed7aec4e42db04b9c006070
meta-rivak-shah <52689827+meta-rivak-shah@users.noreply.github.com>
2019-07-23 12:17:31 +0530

    Update MultiplyPolyTestCases.java

--- Assignment6/Polynomail/MultiplyPolyTestCases.java
+++ Assignment6/Polynomail/MultiplyPolyTestCases.java
@@ -14,30 +14,30 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class MultiplyPolyTestCases{
 	
-	private int expectedResult[];
+	private String expectedResult;
 	private static Polynomial Polynomial;
 	private static Polynomial Polynomial1;
 	@Before
 	public void initialize() {
-		Polynomial = new Polynomial(new int[]{2,1,1}, new int[]{4,1,0});
+		Polynomial = new Polynomial(new int[]{2,1,1}, new int[]{2,1,0});
 		Polynomial1 = new Polynomial(new int[]{2,1,1}, new int[]{2,1,0});
 		
 	}
 
-	public MultiplyPolyTestCases(int expectedResult[],IntSet IntSet1, IntSet  IntSet2) {
+	public MultiplyPolyTestCases(String expectedResult,IntSet IntSet1, IntSet  IntSet2) {
 	this.expectedResult = expectedResult;
 	}
 
 	@Parameterized.Parameters
 	public static Collection<Object[]> data1() {
 	return Arrays.asList(new Object[][] {
-	{new int[]{4,11,14,10},Polynomial,Polynomial1}
+	{"4x^4 4x^3 5x^2 2x^1 1x^0 ",Polynomial,Polynomial1}
 	});
 	}
 	
 	@Test
 	public void testIntSet1() {
-		assertArrayEquals(expectedResult, Polynomial1.multiPoly(Polynomial, Polynomial1));
+		assertEquals(expectedResult, Polynomial1.multiPoly(Polynomial, Polynomial1));
 	}
 	
 	
