4034d7c851211fc92d09f96e9aff863e6f3fb1ba
meta-rivak-shah <rivak.shah@metacube.com>
2019-08-17 16:58:14 +0530

    My third DBMS Assingment

---
+++ DBMSAssingmnt3/Assignment5.sql
@@ -0,0 +1,49 @@
+/*
+Assignment 4:
+Consider a form where providing a Zip Code populates associated City and
+State. 
+Create appropriate tables and relationships for the same and write a SQL
+         query for that returns a Resultset containing Zip Code, City Names and
+         States ordered by State Name and City Name.
+*/
+
+CREATE TABLE ZIPCODE(
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ZipCode INT NOT NULL
+);
+
+INSERT INTO ZIPCODE(ZipCode) VALUES(314080),(302017),(302018),(303030),(311212),(12345),(223344),(223344),(223344);
+
+SELECT 
+    *
+FROM
+    ZIPCODE;
+
+CREATE TABLE city_state(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    zipcode_id INT,
+    FOREIGN KEY (zipcode_id)
+        REFERENCES zipcode (ID),
+    city char(11),
+    state char(11)
+);  
+
+INSERT INTO city_state(zipcode_id,city,state) 
+VALUES(1,'RAWATBHATA','RAJASTHAN'),
+(2,'MUMBAI','MAHARASHTRA'),
+(3,'JAIPUR','RAJASTHAN'),
+(4,'DELHI','DELHI'),
+(5,'Panji','GOA'),
+(6,'Ahembdabad','Gujrat'),
+(7,'patna','Bihar'),
+(8,'Kishangarh','Rajasthan'),
+(9,'Hyderabad','Telangana');
+
+SELECT 
+    Z.ZipCode, cs.City, cs.State
+FROM
+    ZIPCODE Z
+        LEFT JOIN
+    city_State cs ON cs.zipcode_Id = Z.Id
+ORDER BY cs.state,cs.city;
+
---
+++ DBMSAssingmnt3/Assingment6.sql
@@ -0,0 +1,107 @@
+SELECT * FROM CATEGORY;
+SELECT * FROM PRODUCT;
+SELECT * FROM PRODUCTCategory;
+
+/*
+Display the list of products (Id, Title, Count of Categories) which fall in more than one Categories.
+*/
+SELECT 
+    p.*, Count(DISTINCT pc.CAT_ID) AS TOTAL_CATEGORY
+FROM
+    PRODUCT p
+        LEFT JOIN
+    PRODUCTCATEGORY pc ON p.id = pc.prod_id
+GROUP BY pc.Prod_Id
+Having COUNT(CAT_Id) > 1;
+
+/*
+Display the Categories along with number of products under each category.
+*/
+SELECT 
+    c.*, Count(DISTINCT pc.Prod_Id) AS TOTAL_PRODUCT
+FROM
+    CATEGORY c
+        LEFT JOIN
+    PRODUCTCATEGORY pc ON c.id = pc.Cat_Id
+GROUP BY Cat_Id Having count(PROD_ID) >0;
+
+/*
+Display Count of products as per below price range:
+Range in Rs.
+Count
+0 - 100
+101 - 500
+Above 500
+*/
+SELECT 
+    price, count(*) no_of_products
+FROM
+    (SELECT 
+        CASE
+                WHEN price >= 0 AND price <= 100 THEN '0-100'
+                WHEN price >= 100 AND price <= 500 THEN '100-500'
+                WHEN price >= 500 THEN '500>'
+            END AS PRICE
+    FROM
+        PRODUCT) AS count_of_products
+GROUP BY price;
+
+/*
+Assignment 3:
+Write SQL scripts for the following:
+Display Shopper’s information along with number of orders he/she placed during last 30 days.
+*/
+ SELECT 
+    u.*, count(Order_id) as Total_Order
+from
+    orders,
+    user u
+WHERE
+    DATEDIFF(now(), orders.orderDate) < 30 AND u.Id = orders.user_id
+    GROUP BY User_Id;
+    
+    
+SELECT * FROM ORDERS;
+SELECT * FROM ITEM_ORDER;
+SELECT * FROM USER;
+SELECT * FROM PRODUCT;
+/*
+Display the top 10 Shoppers who generated maximum number of revenue in last 30 days.
+*/
+SELECT 
+    U.ID, U.Name, O.User_Id, SUM(I.TotalAmount) AS Total_REVENUE
+FROM
+    ORDERS O,
+    User U,
+    ITEM_ORDER I
+WHERE
+    O.Order_ID = I.ORDER_ID AND DATEDIFF(now(), O.orderDate) < 30 AND O.User_Id = U.ID
+GROUP BY O.USER_ID DESC
+LIMIT 10;
+
+/*
+Given a category search keyword, display all the Products present in this category/categories
+*/
+SELECT 
+    product.*
+FROM
+    product,
+    ProductCategory,
+    CATEGORY
+Where
+    product.id = ProductCategory.prod_id AND CATEGORY.type = 't-shirt' AND CATEGORY.id = ProductCategory.cat_id;
+
+/*
+Mark the products as Inactive which are not ordered in last 90 days.
+*/
+SELECT 
+    *
+FROM
+    PRODUCT
+WHERE
+    ID NOT IN (SELECT DISTINCT
+        PRODUCT_ID
+    FROM
+        ITEM_ORDER I
+            LEFT JOIN
+        ORDERS O ON O.ORDER_ID = I.ORDER_ID AND DATEDIFF(now(), O.orderDate) < 90);
